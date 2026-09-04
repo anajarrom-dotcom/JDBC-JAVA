68cfcd508512e9f4bf90c684db6e59e87aaa0461
Antony Najarro <anajarrom@miumg.edu.gt>
2026-09-04 16:42:07 -0600

    Ejercicio 1

--- edu/umg/programacion2/clase07/biblioteca/dao/LibroDAO.java
+++ edu/umg/programacion2/clase07/biblioteca/dao/LibroDAO.java
@@ -22,7 +22,7 @@ public class LibroDAO {
 
     private static final String URL = "jdbc:mysql://localhost:3306/prog2_db?useSSL=false&serverTimezone=UTC";
     private static final String USUARIO = "root";
-    private static final String PASSWORD = "tu_password_aqui";
+    private static final String PASSWORD = "Tonyñi123.";
 
     public int crear(Libro libro) throws SQLException {
         String sql = "INSERT INTO libros (titulo, autor, isbn) VALUES (?, ?, ?)";
